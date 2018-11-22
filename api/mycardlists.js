adf0001a1046ef0f4ec3c2bee2d383d55b893dd7
cardoptimizer <jaepyojung@koreanair.com>
2018-11-22 14:57:29 +0900

    api

--- api/mycardlists.js
+++ api/mycardlists.js
@@ -7,10 +7,10 @@ var mongoose = require('mongoose');
 router.get('/',
   function(req, res, next){
     var query = {};
-    if(req.query.name) query.name = {$regex:req.query.name, $options:'i'};
+    if(req.query.name) query.person_name = {$regex:req.query.name, $options:'i'};
 
     mycardlist.find(query)
-    .sort({id: 1})
+    .sort({person_name: 1})
     .exec(function(err, mycardlists){
       if(err) {
         res.status(500);
@@ -23,23 +23,5 @@ router.get('/',
   }
 );
 
-// Show
-router.get('/:id',
-  function(req, res, next){
-    mycardlist.findOne({id:req.params.id})
-    .exec(function(err, mycardlist){
-      if(err) {
-        res.status(500);
-        res.json({success:false, message:err});
-      }
-      else if(!mycardlist){
-        res.json({success:false, message:"mycardlist not found"});
-      }
-      else {
-        res.json({success:true, data:mycardlist});
-      }
-    });
-  }
-);
 
 module.exports = router;
